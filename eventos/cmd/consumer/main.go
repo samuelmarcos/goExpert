e507e19accf852127afdb5e792a80f33cfef4767
samuelmarcos <samuelmarcos9580@gmail.com>
2025-04-24 10:05:59 -0300

    chore: improve code

--- eventos/cmd/consumer/main.go
+++ eventos/cmd/consumer/main.go
@@ -1,24 +1,100 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/samuelmarscos/eventos/pkg/rabbitmq"
 	"github.com/streadway/amqp"
 )
 
-func main() {
-	ch, err := rabbitmq.OpenChannel()
+const (
+	defaultQueueName = "orders"
+	prefetchCount    = 10 // Number of messages to prefetch
+)
 
+func main() {
+	// Create a context that will be canceled on interrupt
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// Initialize logger
+	logger := log.New(os.Stdout, "[CONSUMER] ", log.LstdFlags)
+
+	// Open RabbitMQ channel
+	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
-		panic(err)
+		logger.Fatalf("Failed to open channel: %v", err)
 	}
 	defer ch.Close()
 
-	msgs := make(chan amqp.Delivery)
-	go rabbitmq.Consume(ch, msgs, "orders")
-	for msg := range msgs {
-		fmt.Println(string(msg.Body))
-		msg.Ack(false)
+	// Set QoS to control how many messages are consumed at once
+	err = ch.Qos(
+		prefetchCount, // prefetch count
+		0,             // prefetch size
+		false,         // global
+	)
+	if err != nil {
+		logger.Fatalf("Failed to set QoS: %v", err)
 	}
+
+	// Create message channel
+	msgs := make(chan amqp.Delivery)
+
+	// Start consuming messages
+	logger.Printf("Starting to consume messages from queue '%s'", defaultQueueName)
+	go rabbitmq.Consume(ch, msgs, defaultQueueName)
+
+	// Create a ticker for logging
+	ticker := time.NewTicker(30 * time.Second)
+	defer ticker.Stop()
+
+	// Process messages
+	var processedCount int
+	for {
+		select {
+		case <-ctx.Done():
+			logger.Printf("Received shutdown signal. Processed %d messages", processedCount)
+			return
+		case <-ticker.C:
+			logger.Printf("Consumer is running. Processed %d messages so far", processedCount)
+		case msg, ok := <-msgs:
+			if !ok {
+				logger.Println("Message channel closed")
+				return
+			}
+
+			// Process the message
+			if err := processMessage(logger, msg); err != nil {
+				logger.Printf("Failed to process message: %v", err)
+				// Reject the message and don't requeue it
+				if err := msg.Reject(false); err != nil {
+					logger.Printf("Failed to reject message: %v", err)
+				}
+				continue
+			}
+			processedCount++
+
+			// Acknowledge the message only if processing was successful
+			if err := msg.Ack(false); err != nil {
+				logger.Printf("Failed to acknowledge message: %v", err)
+			}
+		}
+	}
+}
+
+func processMessage(logger *log.Logger, msg amqp.Delivery) error {
+	// Add your message processing logic here
+	logger.Printf("Received message: %s", string(msg.Body))
+
+	// Simulate some processing time
+	time.Sleep(100 * time.Millisecond)
+
+	// Return nil if processing was successful
+	// Return an error if processing failed
+	return nil
 }
--- eventos/cmd/producer/main.go
+++ eventos/cmd/producer/main.go
@@ -1,23 +1,61 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/samuelmarscos/eventos/pkg/rabbitmq"
 )
 
+const (
+	defaultMessageCount = 100000
+	defaultExchange     = "amq.direct"
+)
+
 func main() {
+	// Create a context that will be canceled on interrupt
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
 
+	// Initialize logger
+	logger := log.New(os.Stdout, "[PRODUCER] ", log.LstdFlags)
+
+	// Open RabbitMQ channel
 	ch, err := rabbitmq.OpenChannel()
-
 	if err != nil {
-		panic(err)
+		logger.Fatalf("Failed to open channel: %v", err)
 	}
 	defer ch.Close()
 
-	for i := 0; i < 100000; i++ {
-		msg := fmt.Sprintf("Esse é o %d Hello World !", i)
-		rabbitmq.Publish(ch, msg, "amq.direct")
+	// Create a ticker for logging progress
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	// Start publishing messages
+	logger.Printf("Starting to publish %d messages to exchange '%s'", defaultMessageCount, defaultExchange)
+
+	var publishedCount int
+	for i := 0; i < defaultMessageCount; i++ {
+		select {
+		case <-ctx.Done():
+			logger.Printf("Received shutdown signal. Published %d messages", publishedCount)
+			return
+		case <-ticker.C:
+			logger.Printf("Progress: %d/%d messages published", publishedCount, defaultMessageCount)
+		default:
+			msg := fmt.Sprintf("Message %d: Hello World!", i)
+			if err := rabbitmq.Publish(ch, msg, defaultExchange); err != nil {
+				logger.Printf("Failed to publish message %d: %v", i, err)
+				continue
+			}
+			publishedCount++
+		}
 	}
 
+	logger.Printf("Successfully published all %d messages", publishedCount)
 }
