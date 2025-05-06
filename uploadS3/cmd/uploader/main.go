c2b3bcaa5d8881c4dcef1489f37c41f6727b9a77
samuelmarcos <samuelmarcos9580@gmail.com>
2025-05-06 11:17:13 -0300

    feat: upload s3 first version

---
+++ uploadS3/cmd/uploader/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var (
+	s3Client *s3.S3
+	s3Bucket string
+	wg       sync.WaitGroup
+)
+
+func init() {
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String("us-east-1"),
+			Credentials: credentials.NewStaticCredentials(
+				"",
+				"",
+				"",
+			),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	s3Client = s3.New(sess)
+	s3Bucket = "goexpert-bucket-exemplo"
+}
+
+func main() {
+	dir, err := os.Open("./temp")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+	for {
+		files, err := dir.ReadDir(1)
+		if err == nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("Error reading directory: %s\n", err)
+			continue
+		}
+		wg.Add(1)
+		go uploadFile(files[0].Name())
+	}
+
+	wg.Wait()
+}
+
+func uploadFile(fileName string) {
+	defer wg.Done()
+	completeFileName := fmt.Sprintf("./tmp/%s", fileName)
+
+	fmt.Printf("Uploading file %s to bucket %s", completeFileName, s3Bucket)
+
+	f, err := os.Open(completeFileName)
+	if err != nil {
+		fmt.Printf("Error opening file %s\n", completeFileName)
+		return
+	}
+	defer f.Close()
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(fileName),
+		Body:   f,
+	})
+	if err != nil {
+		fmt.Printf("Error uploading file %s\n", completeFileName)
+		return
+	}
+	fmt.Printf("File %s uplaoded successfuly\n", completeFileName)
+}
---
+++ uploadS3/go.mod
@@ -0,0 +1,8 @@
+module github.com/goExpert/s3Upload
+
+go 1.22.5
+
+require (
+	github.com/aws/aws-sdk-go v1.55.7 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
