1ed96c92fbbe148caaa3a9475a82cdb23465c230
samuelmarcos <samuelmarcos9580@gmail.com>
2025-08-21 20:50:16 -0300

    fix: list query

--- desafio3/internal/infra/database/order_repository.go
+++ desafio3/internal/infra/database/order_repository.go
@@ -36,12 +36,12 @@ func (r *OrderRepository) GetTotal() (int, error) {
 }
 
 func (r *OrderRepository) ListOrder() ([]entity.Order, error) {
-	rows, err := r.Db.Query("SELECT * FROM orders LIMIT ? OFFSET ?")
+	rows, err := r.Db.Query("SELECT id, price, tax, final_price FROM orders")
 	if err != nil {
 		return nil, err
 	}
-
 	defer rows.Close()
+
 	var orders []entity.Order
 	for rows.Next() {
 		var order entity.Order
